3e4695a8fad6279f4ae3f721c75bba6eba022743
pro <>
2018-04-20 00:16:00 -0700

    1. Encapsulate system call with _withError

--- server.c
+++ server.c
@@ -1,6 +1,6 @@
 //
-//  lab1.c
-//  Project_1A
+//  server.c
+//  Project_1B
 //  CS 111
 //  Created by Zegao on 4/12/18.
 //  Copyright © 2018 pro. All rights reserved.
@@ -20,6 +20,9 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 char isThereShell = 'N';
 
@@ -76,6 +79,45 @@ void changeInputMode(void)
     tcsetattr (0, TCSANOW, &tattr);
 }
 
+
+void closeWithError(int pipe)
+{
+    int RC = close(pipe);
+    if (RC == -1){
+        fprintf(stderr, "Error:%s.\r\n", strerror(errno));
+        exit(1);
+    }
+}
+
+void dup2WithError(int pipe, int fd)
+{
+    int RC = dup2(pipe,fd);
+    if (RC == -1){
+        fprintf(stderr, "Error:%s.\r\n", strerror(errno));
+        exit(1);
+    }
+}
+
+int readWithError(int fd, char* buffer, int size)
+{
+    int RC = read(fd, buffer, size);
+    if (RC == -1){
+        fprintf(stderr, "Error:%s.\r\n", strerror(errno));
+        exit(1);
+    }
+    return RC;
+}
+
+int writeWithError(int fd, char* buffer, int size)
+{
+    int RC = write(fd, buffer, size);
+    if (RC == -1){
+        fprintf(stderr, "Error:%s.\r\n", strerror(errno));
+        exit(1);
+    }
+    return RC;
+}
+
 int main(int argc, char * argv[]) {
     
     if (signal(SIGPIPE, catch) == SIG_ERR ){
@@ -100,26 +142,14 @@ int main(int argc, char * argv[]) {
         char buffer = '\0';
         char temp_buffer[2] = "\0";
         while((buffer != '\03')&&(buffer != '\04')){
-            int RC = read(0,&buffer, sizeof(char));
-            if (RC == -1){
-                fprintf(stderr, "Error:%s.\r\n", strerror(errno));
-                exit(1);
-            }
+            readWithError(0,&buffer, sizeof(char));
             //Deal with the line change problem
             if(buffer == '\r' || buffer == '\n'){
                 temp_buffer[0] = '\r';
                 temp_buffer[1] = '\n';
-                RC = write(1,temp_buffer,sizeof(temp_buffer));
-                if (RC == -1){
-                    fprintf(stderr, "Error:%s.\r\n", strerror(errno));
-                    exit(1);
-                }
+                writeWithError(1,temp_buffer,sizeof(temp_buffer));
             }else{
-                RC = write(1,&buffer,sizeof(char));
-                if (RC == -1){
-                    fprintf(stderr, "Error:%s.\r\n", strerror(errno));
-                    exit(1);
-                }
+                writeWithError(1,&buffer,sizeof(char));
             }
         }
     }else{//Shell Mode
@@ -139,43 +169,13 @@ int main(int argc, char * argv[]) {
             fprintf(stderr, "Error:%s.\r\n", strerror(errno));
             exit(1);
         }else if (fpid == 0){//Child process
-            //Close useless fd
-            int RC = close(toChildPip[1]);
-            if (RC == -1){
-                fprintf(stderr, "Error:%s.\r\n", strerror(errno));
-                exit(1);
-            }
-            RC = close(toParentPip[0]);
-            //Dup fd to stdin and stdout
-            if (RC == -1){
-                fprintf(stderr, "Error:%s.\r\n", strerror(errno));
-                exit(1);
-            }
-            RC = dup2(toChildPip[0],0);
-            if (RC == -1){
-                fprintf(stderr, "Error:%s.\r\n", strerror(errno));
-                exit(1);
-            }
-            RC = close(toChildPip[0]);
-            if (RC == -1){
-                fprintf(stderr, "Error:%s.\r\n", strerror(errno));
-                exit(1);
-            }
-            RC = dup2(toParentPip[1],1);
-            if (RC == -1){
-                fprintf(stderr, "Error:%s.\r\n", strerror(errno));
-                exit(1);
-            }
-            RC = dup2(toParentPip[1],2);
-            if (RC == -1){
-                fprintf(stderr, "Error:%s.\r\n", strerror(errno));
-                exit(1);
-            }
-            RC = close(toParentPip[1]);
-            if (RC == -1){
-                fprintf(stderr, "Error:%s.\r\n", strerror(errno));
-                exit(1);
-            }
+            closeWithError(toChildPip[1]);
+            closeWithError(toParentPip[0]);
+            dup2WithError(toChildPip[0],0);
+            closeWithError(toChildPip[0]);
+            dup2WithError(toParentPip[1],1);
+            dup2WithError(toParentPip[1],2);
+            closeWithError(toParentPip[1]);
             char *execvp_argv[2];
             char execvp_filename[] = "/bin/bash";
             execvp_argv[0] = execvp_filename;
@@ -186,16 +186,8 @@ int main(int argc, char * argv[]) {
             }
         }else{//parent process
             //Close useless fd
-            int RC = close(toChildPip[0]);
-            if (RC == -1){
-                fprintf(stderr, "Error:%s.\r\n", strerror(errno));
-                exit(1);
-            }
-            RC = close(toParentPip[1]);
-            if (RC == -1){
-                fprintf(stderr, "Error:%s.\r\n", strerror(errno));
-                exit(1);
-            }
+            closeWithError(toChildPip[0]);
+            closeWithError(toParentPip[1]);
             changeInputMode();
             char parentbuffer;
             char newbuffer[2048] = "\0";
@@ -215,10 +207,7 @@ int main(int argc, char * argv[]) {
                     exit(1);
                 }
                 if ((pollFdGroup[0].revents & POLLIN)){
-                    if(read(0,&parentbuffer, 1)<0){
-                        fprintf(stderr, "Error: failed parent stdin \r\n");
-                        exit(1);
-                    }
+                    readWithError(0,&parentbuffer, 1);
                     if(parentbuffer == '\04'){
                         close(toChildPip[1]);
                         //exit(0);
@@ -227,58 +216,23 @@ int main(int argc, char * argv[]) {
                         //exit(0);
                     }else if(parentbuffer == '\r' || parentbuffer  == '\n'){
                         char tempBuffer = {'\n'};
-                        int writeRC =  write(toChildPip[1],&tempBuffer,sizeof(tempBuffer));
-                        int errorNote = errno;
-                        if( writeRC < 0 ){
-                            fprintf(stderr, "Error:%s.\r\n", strerror(errorNote));
-                            exit(1);
-                        }
+                        writeWithError(toChildPip[1],&tempBuffer,sizeof(tempBuffer));
                         char templineBuffer[2] = {'\r','\n'};
-                        writeRC = write(1,templineBuffer,sizeof(templineBuffer));
-                        errorNote = errno;
-                        if( writeRC < 0 ){
-                            fprintf(stderr, "Error:%s.\r\n", strerror(errorNote));
-                            exit(1);
-                        }
+                        writeWithError(1,templineBuffer,sizeof(templineBuffer));
                     }else{
-                        int writeRC = write(toChildPip[1], &parentbuffer, sizeof(parentbuffer));
-                        int errorNote = errno;
-                        if( writeRC < 0 ){
-                            fprintf(stderr, "Error:%s.\r\n", strerror(errorNote));
-                            exit(1);
-                        }
-                        writeRC = write(1,&parentbuffer,sizeof(parentbuffer));
-                        errorNote = errno;
-                        if( writeRC < 0 ){
-                            fprintf(stderr, "Error:%s.\r\n", strerror(errorNote));
-                            exit(1);
-                        }
+                        writeWithError(toChildPip[1], &parentbuffer, sizeof(parentbuffer));
+                        writeWithError(1,&parentbuffer,sizeof(parentbuffer));
                     }
                 }
                 if ((pollFdGroup[1].revents & POLLIN)) {
-                    int count = read(toParentPip[0], newbuffer, 2048); // read from shell pipe
-                    int errorNote = errno;
-                    if( count < 0 ){
-                        fprintf(stderr, "Error:%s.\r\n", strerror(errorNote));
-                        exit(1);
-                    }
+                    int count = readWithError(toParentPip[0], newbuffer, 2048); // read from shell pipe
                     int i;
                     for(i = 0;i<count;i++){
                         if(newbuffer[i] == '\n'){
                             char tempshellBuffer[2] = {'\r','\n'};
-                            int writeRC  = write(1,&tempshellBuffer,sizeof(tempshellBuffer));
-                            errorNote = errno;
-                            if( writeRC < 0 ){
-                                fprintf(stderr, "Error:%s.\r\n", strerror(errorNote));
-                                exit(1);
-                            }
+                            writeWithError(1,tempshellBuffer,sizeof(tempshellBuffer));
                         }else{
-                            int writeRC = write(1,&newbuffer[i],1);
-                            errorNote = errno;
-                            if( writeRC < 0 ){
-                                fprintf(stderr, "Error:%s.\r\n", strerror(errorNote));
-                                exit(1);
-                            }
+                            writeWithError(1,&newbuffer[i],1);
                         }
                     }
                 }
@@ -291,5 +245,3 @@ int main(int argc, char * argv[]) {
     }
     exit(0);
 }
-
-
